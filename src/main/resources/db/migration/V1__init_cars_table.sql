b6b337e339b3700895182c9abd25b6a6c9527855
Robert <cj92robert@gmail.com>
2020-08-31 20:03:18 +0200

    fix migration

--- src/main/resources/db/migration/V1__init_cars_table.sql
+++ src/main/resources/db/migration/V1__init_cars_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cars(
-                     id int primary key,
+                     id int primary key AUTO_INCREMENT,
                      model varchar(50) not null,
                      car_make varchar(50) not null,
                      colour varchar(30) null,
